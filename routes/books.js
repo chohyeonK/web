e9f2dafcaf1832ccbbfb3c3094d55ef2b315f9bd
chohyeon <chohyeonK3@gmail.com>
2023-10-28 17:45:57 +0900

    week07-09

--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '도서검색', pageName: 'books/search.ejs' });
 });
 
+/* 장바구니 page. */
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: '장바구니', pageName: 'books/cart.ejs' }); // 시험
+});
 module.exports = router;
