ef862bd45c13ae7355829d4f0c13f4290c5bdd29
chohyeon <chohyeonK3@gmail.com>
2023-11-25 18:09:04 +0900

    week 13

--- routes/posts.js
+++ routes/posts.js
@@ -11,4 +11,15 @@ router.get('/insert', function(req, res, next) {
     res.render('index', { title: '글쓰기', pageName: 'posts/insert.ejs' });
 });
 
+/* 글읽기 페이지 */
+router.get('/read', function(req, res, next) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글 정보', pageName: 'posts/read.ejs', id }); //id:id
+  });
+
+router.get('/update', function(req, res, next) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글 수정', pageName: 'posts/update.ejs', id }); //id:id
+  });
+
 module.exports = router;
